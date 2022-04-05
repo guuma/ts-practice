bc94c7c947760c1ce097c536dd1cad316b368725
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2022-04-06 06:57:35 +0900

    fix TupleToObject

--- src/types/TupleToObject.ts
+++ src/types/TupleToObject.ts
@@ -1,7 +1,23 @@
-// expect to TupleToObject<["BUY", "SELL"]>
+// const orderSide = ["BUY", "SELL"] as const
+// expect to TupleToObject<typeof orderSide>
 // => {"BUY": "BUY", "SELL": "SELL"}
 
 export type TupleToObject<T extends readonly any[]> = {
   [K in T[number]]: K;
 };
 
+const orderSide = ['BUY', 'SELL'] as const;
+
+const orderSideObj: TupleToObject<typeof orderSide> = {
+  BUY: 'BUY',
+  SELL: 'SEL',
+};
+// The typo is Error
+
+const notConsOrderSide = ['BUY', 'SELL'];
+
+const notConsOrderSideObj: TupleToObject<typeof notConsOrderSide> = {
+  BUY: 'BUY',
+  SELL: 'SELLL',
+};
+// The typo is not Error
