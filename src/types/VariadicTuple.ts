702db525f2ba5057fee515413547ce22a2a60ba9
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2022-04-12 08:54:33 +0900

    Add comment in VariadicTuple

--- src/types/VariadicTuple.ts
+++ src/types/VariadicTuple.ts
@@ -1,7 +1,5 @@
+//  expect to be Tail<['banana', 'orange', 'apple']> => ["orange", "apple"]
 type Tail<T extends [any, ...any[]]> = T extends readonly [any, ...infer U] ? U : never;
 
+// expect to be Head<['banana', 'orange', 'apple']> => ["banana", "orange"]
 type Head<T extends [...any[], any]> = T extends readonly [...infer U, any] ? U : never;
-
-type TailOfArray = Tail<['banana', 'orange', 'apple']>;
-
-type HeadOfArray = Head<['banana', 'orange', 'apple']>;
